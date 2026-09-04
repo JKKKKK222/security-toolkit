7073909fd6dcd5c24dfebbbb3fe051cfa7331e6c
JKKKKK222 <2802415933@qq.com>
2026-09-04 13:07:07 +0800

    feat: add password character checks

--- main.py
+++ main.py
@@ -1,7 +1,31 @@
-from modules.password_analyzer import check_password_length
+from modules.password_analyzer import check_password_length, check_has_digit, check_has_uppercase, check_has_lowercase, check_has_special
 password = input("请输入一个密码：")
-result = check_password_length(password)
-if result:
+result_length = check_password_length(password)
+if result_length:
     print("密码长度符合基本要求。")
 else:
     print("密码长度过短，建议至少使用 8 个字符。")
+
+result_has_digit = check_has_digit(password)
+if result_has_digit:
+    print("密码中包含数字字符")
+else:
+    print("密码中不包含数字字符")
+
+result_has_uppercase = check_has_uppercase(password)
+if result_has_uppercase:
+    print("密码中包含大写字母")
+else:
+    print("密码中不包含大写字母")
+
+result_has_lowercase = check_has_lowercase(password)
+if result_has_lowercase:
+    print("密码中包含小写字母")
+else:
+    print("密码中不包含小写字母")
+
+result_has_special = check_has_special(password)
+if result_has_special:
+    print("密码中包含特殊字符")
+else:
+    print("密码中不包含特殊字符")
--- modules/password_analyzer.py
+++ modules/password_analyzer.py
@@ -1,5 +1,31 @@
+import string
+
 def check_password_length(password):
     if len(password) >= 8:
         return True
     else:
         return False
+
+def check_has_digit(password):
+    for char in password:
+        if char.isdigit():
+            return True
+    return False
+
+def check_has_uppercase(password):
+    for char in password:
+        if char.isupper():
+            return True
+    return False
+
+def check_has_lowercase(password):
+    for char in password:
+        if char.islower():
+            return True
+    return False
+
+def check_has_special(password):
+    for char in password:
+        if char in string.punctuation:
+            return True
+    return False
