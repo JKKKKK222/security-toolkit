1c3cf3aa38bf2caeb9d8b6940df41159a49935f0
JKKKKK222 <2802415933@qq.com>
2026-09-04 12:27:15 +0800

    feat: add password length validation

--- main.py
+++ main.py
@@ -1,1 +1,7 @@
-print("Security Toolkit")
+from modules.password_analyzer import check_password_length
+password = input("请输入一个密码：")
+result = check_password_length(password)
+if result:
+    print("密码长度符合基本要求。")
+else:
+    print("密码长度过短，建议至少使用 8 个字符。")
---
+++ modules/password_analyzer.py
@@ -0,0 +1,5 @@
+def check_password_length(password):
+    if len(password) >= 8:
+        return True
+    else:
+        return False
