768231c228c77895095df76b9bf9a571cb6eeca6
JKKKKK222 <2802415933@qq.com>
2026-09-04 15:56:07 +0800

    refactor: simplify password check display logic

--- main.py
+++ main.py
@@ -1,31 +1,42 @@
-from modules.password_analyzer import check_password_length, check_has_digit, check_has_uppercase, check_has_lowercase, check_has_special
+from modules.password_analyzer import check_password_length, check_has_digit, check_has_uppercase, check_has_lowercase, check_has_special, calculate_score, get_strength
+
 password = input("请输入一个密码：")
-result_length = check_password_length(password)
-if result_length:
-    print("密码长度符合基本要求。")
-else:
-    print("密码长度过短，建议至少使用 8 个字符。")
 
-result_has_digit = check_has_digit(password)
-if result_has_digit:
-    print("密码中包含数字字符")
-else:
-    print("密码中不包含数字字符")
+def display_result(result, success_message, failure_message):
+    if result:
+        print(success_message)
+    else:
+        print(failure_message)
 
-result_has_uppercase = check_has_uppercase(password)
-if result_has_uppercase:
-    print("密码中包含大写字母")
-else:
-    print("密码中不包含大写字母")
+checks = [
+    (check_password_length,
+     "密码长度符合基本要求。",
+     "密码长度过短，建议至少使用 8 个字符。"
+     ),
+    (check_has_digit,
+     "密码中包含数字字符",
+     "密码中不包含数字字符"
+     ),
+    (check_has_uppercase,
+     "密码中包含大写字母",
+     "密码中不包含大写字母"
+     ),
+    (check_has_lowercase,
+     "密码中包含小写字母",
+     "密码中不包含小写字母"
+     ),
+    (check_has_special,
+     "密码中包含特殊字符",
+     "密码中不包含特殊字符"
+    )
+]
 
-result_has_lowercase = check_has_lowercase(password)
-if result_has_lowercase:
-    print("密码中包含小写字母")
-else:
-    print("密码中不包含小写字母")
+for function, success_message, failure_message in checks:
+    result = function(password)
+    display_result(result, success_message, failure_message)
 
-result_has_special = check_has_special(password)
-if result_has_special:
-    print("密码中包含特殊字符")
-else:
-    print("密码中不包含特殊字符")
+print()
+score = calculate_score(password)
+print(f'密码得分:{score}/5')
+print(f'密码强度:{get_strength(password)}')
+
--- modules/password_analyzer.py
+++ modules/password_analyzer.py
@@ -29,3 +29,26 @@ def check_has_special(password):
         if char in string.punctuation:
             return True
     return False
+
+def calculate_score(password):
+    score = 0
+    if check_password_length(password):
+        score += 1
+    if check_has_digit(password):
+            score += 1
+    if check_has_uppercase(password):
+            score += 1
+    if check_has_lowercase(password):
+            score += 1
+    if check_has_special(password):
+            score += 1
+    return score
+
+def get_strength(password):
+    score=calculate_score(password)
+    if score <= 2:
+        return "Weak"
+    elif score == 5:
+        return "Strong"
+    else:
+        return "Medium"
